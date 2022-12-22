fd465069c5adbd71e0b876913b874d94b27d4d76
Ben Arnao <arnaoben@gmail.com>
2022-12-22 12:25:36 -0500

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,24 +1,29 @@
+import numpy as np
 import pandas as pd
 
 
-def will_surpass_best(target, optimization_history, min_delta, min_steps=5, direction='min'):
-    if len(optimization_history) < min_steps:  # not enough data
+def will_surpass_best(target, optimization_history, min_delta=1e-1, min_steps=5, direction='min'):
+    if min_steps < 4 or len(optimization_history) < 4:
+        raise Exception('need at least 4 data points')
+    if len(optimization_history) < min_steps or pd.isna(target):  # not enough data
         return True
 
+    # smooth the vector enough to properly analyze
     smooth_window = 1
     complete = False
     while not complete:
         temp = list(pd.Series(optimization_history).rolling(smooth_window).mean().dropna())
         if len(temp) == 2:  # cannot be smoothed any further
-            optimization_history = temp
+            smoothed_history = temp
             break
         needs_further_smoothing = False
-        
+
         for x in range(len(temp) - 3):
             roc_one = temp[x + 1] - temp[x]
             roc_two = temp[x + 2] - temp[x + 1]
             roc_three = temp[x + 3] - temp[x + 2]
-            
+
+            # make sure that slope is going in the same direction among 3 slope points (4 data points)
             if (roc_one < roc_two and roc_two > roc_three) or \
                     (roc_one > roc_two and roc_two < roc_three):
                 needs_further_smoothing = True
@@ -26,32 +31,21 @@ def will_surpass_best(target, optimization_history, min_delta, min_steps=5, dire
         if needs_further_smoothing:
             smooth_window += 1
         else:
-            optimization_history = temp
+            smoothed_history = temp
             complete = True
 
-    curr_val = optimization_history[-1]
-
-    def get_dx(arr):
-        return [arr[x + 1] - arr[x] for x in range(len(arr) - 1)]
-
-    dx = []
-    while len(optimization_history) > 1:  # calculate dx orders (0 index is first dx)
-        optimization_history = get_dx(optimization_history)
-        dx.append(optimization_history)
-
-    dx = [d[-1] for d in dx]  # get most recent elem of each dx order
-
+    x = [x for x in range(len(smoothed_history))]
+    poly = np.polyfit(x, smoothed_history, deg=2)  # smoothed optimization curve needs 2nd degree
+    curr_x = x[-1] + 1
+    prev_y = None
+    # now we step forward and simulate future optimization curve
     while True:
-        if curr_val >= target:  # has potential to reach best
+        curr_x += 1
+        curr_y = np.polyval(poly, curr_x)
+        if curr_y > target:  # simulated curve has passed target, return True
             return True
-
-        # get predicted slope of *next* step
-        for x in reversed(range(len(dx) - 1)):
-            dx[x] += dx[x + 1]
-
-        # if step too small or negative slope
-        if abs(dx[0]) < min_delta or (dx[0] < 0 if direction == 'max' else dx[0] > 0):
-            return False
-
-        # use predicted slope to take a step
-        curr_val += dx[0]
+        if prev_y is not None:
+            # stop simulating curve if step is too small, or slope starts going in wrong direction
+            if abs(prev_y - curr_y) < min_delta or curr_y < prev_y if direction == 'max' else curr_y > prev_y:
+                return False
+        prev_y = curr_y
